5ce9b689d487d93b2f4d4e833f317fe5e049a182
std-freejia <std.freejia@gmail.com>
2023-11-12 21:15:48 +0900

    feat: user resource 추가

---
+++ src/users/entities/users.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UsersModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  // @Column({ type: 'enum' })
+  // role: UserRole.
+}
