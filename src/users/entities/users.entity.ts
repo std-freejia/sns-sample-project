a155121178d389287be96447573e702ec5c227ca
std-freejia <std.freejia@gmail.com>
2023-11-15 15:21:32 +0900

    feat: @Column 옵션

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,19 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { RolesEnum } from '../const/roles.const';
 
 @Entity()
 export class UsersModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ length: 20, unique: true, }) // 최대 길이 20. 유니크 값. 
   nickname: string;
 
-  @Column()
+  @Column({ unique: true, }) // 유니크 값.
   email: string;
 
   @Column()
   password: string;
 
-  // @Column({ type: 'enum' })
-  // role: UserRole.
+  @Column({ enum: Object.values(RolesEnum), default: RolesEnum.USER })
+  role: RolesEnum;
 }
