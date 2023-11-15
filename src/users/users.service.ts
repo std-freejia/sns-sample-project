11a88fa45dd768f94cbacdacbc511308ab84f1db
std-freejia <std.freejia@gmail.com>
2023-11-15 16:14:01 +0900

    feat: users controller, service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersModel } from './entities/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersModel)
+    private readonly userRepo: Repository<UsersModel>,
+  ) { }
+
+  async createUser(nickname: string, email: string, password: string) {
+    const user = this.userRepo.create({
+      nickname, email, password,
+    });
+
+    const newUser = await this.userRepo.save(user);
+
+    return newUser;
+  }
+  async getAllUsers() {
+    return this.userRepo.find();
+  }
+
+}
